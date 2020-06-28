705fa08c5cb334c6762402d1e76245152602f8d7
Joshua Edwards <joshuaedwards@elizabeths-imac.attlocal.net>
2020-06-28 11:43:00 -0500

    additional links and endpoints added

--- server/server.js
+++ server/server.js
@@ -1,40 +1,44 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3333;
 const http = require('http');
 const httpProxy = require('http-proxy');
 const apiProxy = httpProxy.createProxyServer();
 
-const serverSearchbar = 'http://localhost:3001',
+const serverSearchbar = 'http://searchbarricardo2-dev.us-east-2.elasticbeanstalk.com/',
       serverItemImage = 'http://localhost:3002',
-      serverCarousel = 'http://localhost:3003',
-      serverReviews = 'http://localhost:3004'
+      serverCarousel = 'http://NewCarousel-env.eba-irp2rurw.us-east-2.elasticbeanstalk.com',
+      serverReviews = 'http://111111-env.eba-9uquamkj.us-east-2.elasticbeanstalk.com/'
 
 app.use(express.static(path.join(__dirname, './dist')));
 app.use(express.json())
 
-app.get('/searchBar', (req, res) => {
+app.all('/searchBar', (req, res) => {
 
   res.send('contact made from server searchBar');
   apiProxy.web(req, res, {target: serverSearchbar});
 
 });
 
-app.get('/carousel', (req, res) => {
+app.all('/carousel', (req, res) => {
 
   res.send('contact made from server carousel');
   apiProxy.web(req, res, {target: serverCarousel});
 });
 
-app.get('/itemImage', (req, res) => {
+app.all('/itemImage', (req, res) => {
 
   res.send('contact made from server itemImage');
   apiProxy.web(req, res, {target: serverItemImage});
 });
 
-app.get('/reviews', (req, res) => {
+app.all('/reviews', (req, res) => {
 
   res.send('contact made from server reviews');
   apiProxy.web(req, res, {target: serverReviews});
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+
+  console.log('Listening for request for all services')
+});
